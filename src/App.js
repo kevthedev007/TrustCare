af8e3f34ed1421642d6f80c94c2075bb73e7f1e6
Muhammad-Tahir-S <mtz5prif@gmail.com>
2021-04-23 13:00:22 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="h1-mod">WELCOME</h1>
+      <p className="p-mod">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab cumque possimus labore facilis tempora, eos fuga laborum voluptates veniam, quaerat voluptas sit ipsa obcaecati hic! At facilis ut vitae necessitatibus.</p>
+    </div>
+  );
+}
+
+export default App;
