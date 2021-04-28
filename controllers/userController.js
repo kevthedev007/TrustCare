f47d824d522f29aa7ef8b3411b65387589a24b7d
kevthedev007 <63928159+kevthedev007@users.noreply.github.com>
2021-04-28 15:44:19 +0100

    update on authroute

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,8 +22,8 @@ const userController = {
         if(error) return res.json(error.details[0].message);
 
         const email = req.body.email.toLowerCase();
-        const password = req.body.password.toLowerCase();
-        const password2 = req.body.password2.toLowerCase();
+        const password = req.body.password;
+        const password2 = req.body.password2;
         const role = req.body.role.toLowerCase();
 
         //check if email exists in database already
@@ -57,7 +57,7 @@ const userController = {
         
               transporter.sendMail(mailTransport, (error, info) => {
                   if(error) console.log(error);
-                  res.send('Mail has been sent to your email')
+                  res.send('A verification link has been sent to your email')
               });
 
         } catch(err) {
@@ -83,7 +83,7 @@ const userController = {
         // const { error } = loginValidation(req.body);
         // if(error) return res.status(400).send(error.details[0].message);
 
-        const email = req.body.email;
+        const email = req.body.email.toLowerCase();
         const password = req.body.password;
 
         //check if email exists
@@ -99,9 +99,118 @@ const userController = {
         
         //assign jwt token
         const token = jwt.sign({_id: user.rows[0].user_id }, 'sasuke007');
-        console.log(token)
         res.cookie('auth_token', token, {maxAge: 3600 * 1000 * 24 * 365, httpOnly: false});
         res.send('logged in')
+    },
+
+    forgot_password: function async(req, res) {
+        const email = req.body.email.toLowerCase();
+        
+        //check email
+        const checkMail = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+        if(!checkMail.rows[0]) return res.json('Account does not exist')
+
+        //user exists and now create a one-time link valid for 10 minutes
+        const secret = checkMail.rows[0].password + 'sasuke007';
+        const token = jwt.sign({_id: checkMail.rows[0].user_id }, secret, {expiresIn: '10m'});
+
+        //send mail
+        try {
+            let mailTransport = {
+                from: '"noreply" <themocktherapysite@gmail.com>', // sender address
+                to: email, // list of receivers
+                subject: "Reset Password", // Subject line
+                html: ` <h1>Password Reset</h1>
+                        <h2>Hello ${email}</h2>
+                        <p>Please click on the one-time link blow to reset your password. link is valid for 10 minutes</p>
+                        <a href=https://mocktherapy.herokuapp.com/user/reset-password/${checkMail.rows[0].user_id }/${token}>Click here</a>`, // html body
+              };
+        
+              transporter.sendMail(mailTransport, (error, info) => {
+                  if(error) console.log(error);
+                  res.send('Pasword reset link has been sent to your email')
+              });
+
+            } catch(err) {
+                return res.send(err)
+         }
+
+
+    },
+
+    reset_password: async(req, res) => {
+        const {id, token} = req.params;
+
+        //check if this id exists in database
+        const user = await pool.query('SELECT * FROM users where user_id = $1', [id]);
+        if(!user.rows[0]) return res.json('Invalid user');
+
+        //for valid user with id
+        const secret = user.rows[0].password + 'sasuke007';
+        try {
+            const payload = jwt.verify(token, secret);
+            res.redirect('http://localhost:3000/resetPassword/${id}/${token}')
+        } catch(err) {
+           res.send(err)
+        }
+    },
+
+    change_password: (req, res) => {
+        const {id, token} = req.params;
+        const {password, password2} = req.body;
+
+        //check if this id exists in database
+        const user = await pool.query('SELECT * FROM users where user_id = $1', [id]);
+        if(!user.rows[0]) return res.json('Invalid user');
+
+        //compare passwords
+        if(password == password2) return res.json('Password does not match');
+
+        const secret = user.rows[0].password + 'sasuke007';
+        try {
+            const payload = jwt.verify(token, secret);
+            //encrpyt password
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+
+            //insert new password into database
+            const newPassword = await pool.query('UPDATE users SET password = $1 WHERE user_id = $2', [hashedPassword, id]);
+            res.redirect('http://localhost:3000/loginClient')
+        } catch(err) {
+            res.send(err);
+        }
+    },
+
+    resend_mail: async(req, res) => {
+        const email = req.body;
+
+        const mailExist = await pool.query('SELECT * FROM users WHERE email = $1', [email])
+        if(!mailExist.rows[0]) return res.json('Account does not exist');
+
+        //Create confirmation code with email token
+        const token = jwt.sign({email: email}, 'sasuke007');
+
+        try {
+            const changeCODE = await pool.query('UPDATE users SET confirmation_code = $1 WHERE email = $2', [token, email])
+             //SENDING ACTIVATION MAIL
+             let mailTransport = {
+                from: '"noreply" <themocktherapysite@gmail.com>', // sender address
+                to: email, // list of receivers
+                subject: "Account Activation", // Subject line
+                html: ` <h1>Email Confirmation</h1>
+                        <h2>Hello ${email}</h2>
+                        <p>Thank you for subscribing. Please confirm email by clicking on the link below</p>
+                        <a href=https://mocktherapy.herokuapp.com/user/confirmation/${token}>Click here</a>`, // html body
+              };
+        
+              transporter.sendMail(mailTransport, (error, info) => {
+                  if(error) console.log(error);
+                  res.send('A verification link has been sent to your email')
+              });
+
+        } catch(err) {
+            res.status(400).send(err)
+        }
     },
 
     logout: function(req, res) {
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -4,8 +4,19 @@ const verify = require('../verifytoken')
 
 router.post('/signup', userController.postRegister);
 router.post('/signin', userController.postLogin);
-router.get('/confirmation/:token', userController.Confirmation)
+router.get('/confirmation/:token', userController.Confirmation);
 router.get('/logout', userController.logout);
+
+//auth for reset password
+router.post('/forget-password', userController.forgot_password);
+router.get('/reset-password/:id/:token', userController.reset_password);
+router.post('/reset-password/:id/:token', userController.change_password)
+
+//auth for resend confirmation mail
+router.post('/resend-mail', userController.resend_mail);
+
+
+
 router.get('/post', verify, (req, res) => {
     res.send('You can access this data')
 })
