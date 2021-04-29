d120c94dd7bd2561f7256094b1803567c401a6e8
kevthedev007 <63928159+kevthedev007@users.noreply.github.com>
2021-04-29 12:27:01 +0100

    mssg

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -9,7 +9,7 @@ router.get('/logout', userController.logout);
 
 //auth for reset password
 router.post('/forget-password', userController.forgot_password);
-// router.get('/reset-password/:id/:token', userController.reset_password);
+router.get('/reset-password/:id/:token', userController.reset_password);
 router.post('/reset-password/:id/:token', userController.change_password)
 
 //auth for resend confirmation mail
