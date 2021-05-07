0247223195b4840ef10f98209d17a8f798219da3
kevthedev007 <63928159+kevthedev007@users.noreply.github.com>
2021-05-07 11:45:21 +0100

    token

--- routes/clientRoute.js
+++ routes/clientRoute.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const clientController = require('../controllers/clientController');
 const { verify, isClient } = require('../verifytoken');
 
-router.post('/client-survey', verify,clientController.clientSurvey);
+router.post('/client-survey', [verify, isClient],clientController.clientSurvey);
 router.get('/send-therapists', [verify, isClient], clientController.sendTherapists);
 router.post('/pick-therapist', [verify, isClient], clientController.pickTherapist);
 router.get('/client-profile', [verify, isClient], clientController.client_profile);
