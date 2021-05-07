230cb1261bd06508e138156962d132101cb472b4
kevthedev007 <63928159+kevthedev007@users.noreply.github.com>
2021-05-07 14:41:06 +0100

    token

--- controllers/therapistController.js
+++ controllers/therapistController.js
@@ -11,7 +11,7 @@ const therapistController = {
         //insert into client database
         try {
         const info = await pool.query('INSERT INTO therapists (user_id, firstname, lastname, hello, gender, phone_no, gender_preference, specialty, state_of_residence, about_me) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)', [req.user._id, firstname, lastname, hello, gender, phone_no, gender_preference, specialty, state_of_residence]);
-            res.end();
+            res.send('successful');
         } catch(err) {
             res.send(err)
         }
